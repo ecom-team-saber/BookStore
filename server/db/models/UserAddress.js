521a9adb9c1eb4348a80eb87b5775c657a4f8f65
sshih1116 <sshih1116@gmail.com>
2023-03-01 14:24:58 -0800

    changed userAddress data types back

--- server/db/models/UserAddress.js
+++ server/db/models/UserAddress.js
@@ -11,7 +11,7 @@ const UserAddress = db.define("userAddress", {
     allowNull: false,
   },
   postalCode: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.STRING,
     allowNull: false,
   },
   country: {
@@ -19,7 +19,7 @@ const UserAddress = db.define("userAddress", {
     allowNull: false,
   },
   mobile: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.STRING,
     allowNull: false,
   },
 });
