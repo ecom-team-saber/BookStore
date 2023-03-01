39a961b58676806af0308a65fb5c08c545143c8f
Benjamin Agalliu <agalliubenjamin@gmail.com>
2023-03-01 15:34:57 -0500

    added order route

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,3 +1,34 @@
 const express = require("express");
 const router = express.Router();
+
+router.get('/orders', (req, res) => {
+    res.json(orders);
+});
+
+router.get('/orders/:id', (req, res) => {
+    const orderId = parseInt(req.params.id);
+    const order = orders.find(o => o.id === orderId);
+    if (!order) {
+      res.status(404).send('Order not found');
+    } else {
+      res.json(order);
+    }
+});
+
+router.put('/orders/:id', (req, res) => {
+    const orderId = parseInt(req.params.id);
+    const order = orders.find(o => o.id === orderId);
+    if (!order) {
+      res.status(404).send('Order not found');
+    } else {
+      const { status } = req.body;
+      if (!status) {
+        res.status(400).send('Status is required');
+      } else {
+        order.status = status;
+        res.json(order);
+      }
+    }
+});
+
 module.exports = router;
--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -1,3 +1,15 @@
-const Order = null;
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Order = db.define('order', {
+  userID: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM('cart', 'pending', 'complete'),
+    allowNull: false
+  }
+});
 
 module.exports = Order;
