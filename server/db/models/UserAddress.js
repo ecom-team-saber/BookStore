593b29e0002da63fcd870439d920d09cc1086372
sshih1116 <sshih1116@gmail.com>
2023-03-01 12:51:57 -0800

    fixed conflicts in userAddress

--- server/db/models/UserAddress.js
+++ server/db/models/UserAddress.js
@@ -1,27 +1,3 @@
-const Sequelize = require("sequelize");
-const db = require("../db");
-
-const UserAddress = db.define("userAddress", {
-  addressLine1: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  city: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  postalCode: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  country: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  mobile: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+const UserAddress = null;
 
 module.exports = UserAddress;
