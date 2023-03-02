0a27b4f8946d9ddbd9d4a93c8f70eb2500dc7e56
Karsten Fisk <karstenfisk7@gmail.com>
2023-03-02 13:16:24 -0700

    Added PrroductView file

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import ProductView from "./pages/ProductView";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar />
-      {/* <Home />
+      <Navbar />
+      <Home />
       <Login />
-      <Cart /> */}
+      <Cart />
+      <ProductView />
     </div>
   );
 }
