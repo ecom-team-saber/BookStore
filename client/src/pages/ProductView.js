4600395c5f9e5f309b0902875293a64626a9b224
Benjamin Agalliu <agalliubenjamin@gmail.com>
2023-03-03 12:02:23 -0500

    Productview

--- client/src/pages/ProductView.js
+++ client/src/pages/ProductView.js
@@ -1,3 +1,116 @@
-export default function ProductView() {
-  return "ProductView";
+import React, { useState, useEffect }  from "react";
+import axios from "axios"
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBIcon,
+  MDBBtn,
+  MDBRipple,
+} from "mdb-react-ui-kit";
+
+function App() {
+  const [product, setProduct] = useState({
+    title: '',
+    author: '',
+    price: '',
+    description: '',
+  });
+  const productId = 1;
+
+  useEffect(() => {
+    axios.get(`http://localhost:1347/api/products/${productId}`).then((response) => {
+      setProduct(response.data);
+      console.log(response.data)
+    });
+  }, [productId]);
+  
+  console.log(product)
+  return (
+    <MDBContainer fluid className="my-5">
+      <MDBRow className="justify-content-center">
+        <MDBCol md="8" lg="6" xl="4">
+          <MDBCard style={{ borderRadius: "15px" }}>
+            <MDBRipple
+              rippleColor="light"
+              rippleTag="div"
+              className="bg-image rounded hover-overlay"
+            >
+              <MDBCardImage
+                src="https://pictures.abebooks.com/isbn/9780143104933-us.jpg"
+                fluid
+                className="w-100"
+                style={{
+                  borderTopLeftRadius: "15px",
+                  borderTopRightRadius: "15px",
+                }}
+              />
+              <a href="#!">
+                <div className="mask"></div>
+              </a>
+            </MDBRipple>
+            <MDBCardBody className="pb-0">
+              <div className="d-flex justify-content-between">
+                <div>
+                  <p>
+                    <a href="#!" className="text-dark">
+                      {product.title}
+                    </a>
+                  </p>
+                  <p className="small text-muted">{product.author}</p>
+                </div>
+                <div>
+                  <div className="d-flex flex-row justify-content-end mt-1 mb-4 text-danger">
+                    <MDBIcon fas icon="star" />
+                    <MDBIcon fas icon="star" />
+                    <MDBIcon fas icon="star" />
+                    <MDBIcon fas icon="star" />
+                  </div>
+                  <p className="small text-muted">Rated 4.0/5</p>
+                </div>
+              </div>
+            </MDBCardBody>
+            <hr class="my-0" />
+            <MDBCardBody className="pb-0">
+              <div className="d-flex justify-content-between">
+                <p>
+                  <a href="#!" className="text-dark">
+                    ${product.price}
+                  </a>
+                </p>
+                <p className="text-dark">#### 8787</p>
+              </div>
+              <p className="small text-muted">VISA Platinum</p>
+            </MDBCardBody>
+            <hr class="my-0" />
+            <MDBCardBody className="pb-0">
+              <div className="d-flex justify-content-between">
+                <p>
+                  <a href="#!" className="text-dark">
+                    Description
+                  </a>
+                </p>
+              </div>
+              <p className="small text-muted">{product.description}</p>
+            </MDBCardBody>
+            <hr class="my-0" />
+            <MDBCardBody className="pb-0">
+              <div className="d-flex justify-content-between align-items-center pb-2 mb-4">
+                <a href="#!" className="text-dark fw-bold">
+                  Cancel
+                </a>
+                <MDBBtn color="primary">Buy now</MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
+
+export default App;
+
